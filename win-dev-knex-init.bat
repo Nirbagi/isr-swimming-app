dcdeb3b4d22b5f883ba5dc0b24a14604bd1ffafe
Nirbagi <nirbagi@gmail.com>
2022-04-20 11:08:08 +0300

    fix: fixed windows script to run cmds sequentially.

--- win-dev-knex-init.bat
+++ win-dev-knex-init.bat
@@ -1,23 +1,37 @@
-ECHO "--------------------------------------------------------------------"
 ECHO "starting -- recreating tables"
-ECHO "--------------------------------------------------------------------"
-npx knex migrate:rollback --all
-npx knex migrate:latest --env development
-ECHO "--------------------------------------------------------------------"
-ECHO "finished -- recreating tables"
-ECHO "--------------------------------------------------------------------"
-ECHO "starting -- applying seeds"
-ECHO "--------------------------------------------------------------------"
-npx knex seed:run --specific=roles_seed.js --env development
-npx knex seed:run --specific=teams_seed.js --env development
-npx knex seed:run --specific=users_seed.js --env development
-npx knex seed:run --specific=teams_members_seed.js --env development
-npx knex seed:run --specific=trainings_seed.js --env development
-npx knex seed:run --specific=training_videos_seed.js --env development
-npx knex seed:run --specific=training_scores_seed.js --env development
-npx knex seed:run --specific=tournaments_seed.js --env development
-npx knex seed:run --specific=tournaments_scores_seed.js --env development
-npx knex seed:run --specific=announcements_seed.js --env development
-ECHO "--------------------------------------------------------------------"
-ECHO "finished -- applying seeds"
-ECHO "--------------------------------------------------------------------"
+call npx knex migrate:rollback --all && (
+    call npx knex migrate:latest --env development && (
+        ECHO "finished -- recreating tables"
+        ECHO "starting -- applying seeds"
+        call npx knex seed:run --specific=roles_seed.js --env development && (
+            call npx knex seed:run --specific=teams_seed.js --env development && (
+                call npx knex seed:run --specific=users_seed.js --env development && (
+                    call npx knex seed:run --specific=users_seed.js --env development && (
+                        call npx knex seed:run --specific=teams_members_seed.js --env development && (
+                            call npx knex seed:run --specific=trainings_seed.js --env development && (
+                                call npx knex seed:run --specific=training_videos_seed.js --env development && (
+                                    call npx knex seed:run --specific=training_scores_seed.js --env development && (
+                                        call npx knex seed:run --specific=tournaments_seed.js --env development && (
+                                            call npx knex seed:run --specific=tournaments_scores_seed.js --env development && (
+                                                call npx knex seed:run --specific=announcements_seed.js --env development && (
+                                                    ECHO "finished -- applying seeds"
+                                                )
+                                            )
+                                        )
+                                    )
+                                )
+                            )
+                        )
+                    )
+                )
+            )
+        )
+    )
+)
+
+
+
+
+
+
+
