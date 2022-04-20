0eef2cee3d0e44bd4357e2974778c9e3247db13c
Nirbagi <nirbagi@gmail.com>
2022-04-20 11:15:44 +0300

    fix: win script typo

--- win-dev-knex-init.bat
+++ win-dev-knex-init.bat
@@ -6,16 +6,14 @@ call npx knex migrate:rollback --all && (
         call npx knex seed:run --specific=roles_seed.js --env development && (
             call npx knex seed:run --specific=teams_seed.js --env development && (
                 call npx knex seed:run --specific=users_seed.js --env development && (
-                    call npx knex seed:run --specific=users_seed.js --env development && (
-                        call npx knex seed:run --specific=teams_members_seed.js --env development && (
-                            call npx knex seed:run --specific=trainings_seed.js --env development && (
-                                call npx knex seed:run --specific=training_videos_seed.js --env development && (
-                                    call npx knex seed:run --specific=training_scores_seed.js --env development && (
-                                        call npx knex seed:run --specific=tournaments_seed.js --env development && (
-                                            call npx knex seed:run --specific=tournaments_scores_seed.js --env development && (
-                                                call npx knex seed:run --specific=announcements_seed.js --env development && (
-                                                    ECHO "finished -- applying seeds"
-                                                )
+                    call npx knex seed:run --specific=teams_members_seed.js --env development && (
+                        call npx knex seed:run --specific=trainings_seed.js --env development && (
+                            call npx knex seed:run --specific=training_videos_seed.js --env development && (
+                                call npx knex seed:run --specific=training_scores_seed.js --env development && (
+                                    call npx knex seed:run --specific=tournaments_seed.js --env development && (
+                                        call npx knex seed:run --specific=tournaments_scores_seed.js --env development && (
+                                            call npx knex seed:run --specific=announcements_seed.js --env development && (
+                                                ECHO "finished -- applying seeds"
                                             )
                                         )
                                     )
