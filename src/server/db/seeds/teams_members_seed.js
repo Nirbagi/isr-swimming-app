c7d09e61b26e3d4b6e6a1e01b6ead4f6702b2c8e
Nirbagi <nirbagi@gmail.com>
2022-06-10 14:44:06 +0300

    fix: updated user ids

--- src/server/db/seeds/teams_members_seed.js
+++ src/server/db/seeds/teams_members_seed.js
@@ -4,19 +4,19 @@ exports.seed = (knex, Promise) => {
     .then(() => {
       return knex("teams_members").insert([
         {
-          user_id: 3,
+          user_id: 5,
           team_id: 1,
           created_at: knex.fn.now(),
           updated_at: knex.fn.now(),
         },
         {
-          user_id: 4,
+          user_id: 6,
           team_id: 2,
           created_at: knex.fn.now(),
           updated_at: knex.fn.now(),
         },
         {
-          user_id: 5,
+          user_id: 7,
           team_id: 3,
           created_at: knex.fn.now(),
           updated_at: knex.fn.now(),
